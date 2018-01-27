4d1ac7ef5d3cdf467d085ca57bfc1aeb586f275d
Kishan N <kishann380@gmail.com>
2018-01-27 11:41:10 +0530

    Working Cost Function.

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -14,13 +14,13 @@ namespace NeuralNetwork
         public int HiddenLayerLength { get; set; }
         public int OutputLayerSize { get; set; }
         public int TrainingSize { get; set; }
-        public int MyProperty { get; set; }
         public double Lambda { get; set; }
 
-        //double [][,] Theta_t;
-        //double[,] X;
-        //double[,] y;
-
+        //public Matrix<double>[] Theta { get; set; }
+        //public Matrix<double> X { get; set; }
+        //public Matrix<double> y { get; set; }
+        
+        
         Matrix<double> []Theta;
         Matrix<double> X;
         Matrix<double> y;
@@ -30,30 +30,75 @@ namespace NeuralNetwork
 
         public NeuralNetwork()
         {
-            Theta = new Matrix<double>[HiddenLayerLength + 1];
-            Theta[0] = Matrix<double>.Build.Random(HiddenLayerSize, InputLayerSize + 1);
-            Theta[HiddenLayerLength] = Matrix<double>.Build.Random(OutputLayerSize, HiddenLayerSize + 1);
-            for (int i = 1; i < HiddenLayerLength; i++)
-                Theta[i] = Matrix<double>.Build.Random(HiddenLayerSize + 1, HiddenLayerSize);
+            //Theta = new Matrix<double>[HiddenLayerLength + 1];
+            //Theta[0] = Matrix<double>.Build.Random(HiddenLayerSize, InputLayerSize + 1);
+            //Theta[HiddenLayerLength] = Matrix<double>.Build.Random(OutputLayerSize, HiddenLayerSize + 1);
+            //for (int i = 1; i < HiddenLayerLength; i++)
+            //    Theta[i] = Matrix<double>.Build.Random(HiddenLayerSize + 1, HiddenLayerSize);
            
-            Activation = new Matrix<double>[HiddenLayerSize+2];
-            ActivationWithBias = new Matrix<double>[HiddenLayerSize + 2];
-            Z = new Matrix<double>[HiddenLayerSize + 2];
+            //Activation = new Matrix<double>[HiddenLayerSize+2];
+            //ActivationWithBias = new Matrix<double>[HiddenLayerSize + 2];
+            //Z = new Matrix<double>[HiddenLayerSize + 2];
 
-            X.CopyTo(Activation[0]); 
+            //X.CopyTo(Activation[0]); 
+        }
+
+        public void ReadParams(Matrix<double>[] Theta , Matrix<double> X, Matrix<double> y)
+        {
+            this.Theta = Theta;
+            this.X = X;
+            this.y = y;
+
         }
 
         public double Cost()
         {
+            Activation = new Matrix<double>[HiddenLayerLength + 2];
+            ActivationWithBias = new Matrix<double>[HiddenLayerLength + 2];
+            Z = new Matrix<double>[HiddenLayerLength + 2];
+
+            Activation[0] = Matrix<double>.Build.Dense(X.RowCount, X.ColumnCount, (i, j) => X[i, j]);
+//            X.CopyTo(Activation[0]);
             
 
-            for (int i = 0; i < HiddenLayerSize; i++)
+            double cost;
+
+            Matrix<double> Y = Matrix<double>.Build.Dense(TrainingSize, OutputLayerSize, (i, j) => ( y[i, 0] == j ? 1 : 0 ));
+
+            for (int i = 0; i < HiddenLayerLength+1; i++)
             {
-                ActivationWithBias[0] = Matrix<double>.Build.Dense(Activation[0].RowCount, Activation[0].ColumnCount + 1, (x, y) => (y == 0 ? 0 : X[x, y - 1]));
-                Z[1] = Activation[0] * Theta[0].Transpose();
-                Activation[1] = Sigmoid(Z[1]);
+                ActivationWithBias[i] = Matrix<double>.Build.Dense(Activation[i].RowCount, Activation[i].ColumnCount + 1, (x, y) => (y == 0 ? 1 : Activation[i][x, y - 1]));
+                Z[i+1] = ActivationWithBias[i] * Theta[i].Transpose();
+                Activation[i+1] = Sigmoid(Z[i+1]);
             }
+
+            //J=(1.0/m)*sum(sum(-Y.*log(a3)-(1-Y).*log(1-a3)),2) + reg;
+            var lg1 = Activation[HiddenLayerLength + 1].PointwiseLog();
+            var temp2 = -Y.PointwiseMultiply(lg1);
+            var a = (-Y).PointwiseMultiply(Activation[HiddenLayerLength + 1].PointwiseLog());
+            var t = a.RowSums().Sum();
+           // var b = (1 - Y).PointwiseMultiply( (1-Activation[HiddenLayerLength + 1]).PointwiseLog() );
+
+            var b  =  (1 - Y).PointwiseMultiply( Activation[HiddenLayerLength +1].Map( m => (1-m) ).PointwiseLog() );   
+
+            //reg = lambda/(2.0*m)*(sum(sum(Theta1(:,2:size(Theta1,2)).^2),2) + sum(sum(Theta2(:,2:size(Theta2,2)).^2),2));
+
+
             
+
+            double sum=0.0;
+            for (int i = 0; i < HiddenLayerLength + 1; i++)
+                 sum += Theta[i].SubMatrix(0, Theta[i].RowCount, 1, Theta[i].ColumnCount - 1).Map(m => m*m)
+                    .ColumnSums().Sum();
+            
+            double regularization = Lambda / (2.0 * TrainingSize) * sum;
+
+
+            cost = (1.0 / TrainingSize) * (a - b).ColumnSums().Sum() + regularization;
+
+
+
+            return cost;
 
         }
 
--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -1,11 +1,34 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using MathNet.Numerics.LinearAlgebra;
 namespace NeuralNetwork
 {
     class Program
     {
 
-        
+        public static Matrix<double> ReadCsv(string path)
+        {
+            StreamReader stream = new StreamReader(path);
+            var lines = new List<double[]>();
+
+            while (!stream.EndOfStream)
+            {
+                string[] line = stream.ReadLine().Split(',');
+                var lineValues = new double[line.Length];
+
+                for (int i=0;i<line.Length;i++)
+                {
+                    lineValues[i] = double.Parse(line[i]);
+                }
+                lines.Add(lineValues);
+            }
+
+            var data = lines.ToArray();
+
+            return Matrix<double>.Build.Dense(data.Length, data[0].Length, (i, j) => data[i][j]);
+
+        }
 
 
 
@@ -13,9 +36,28 @@ namespace NeuralNetwork
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] a;
+            var Theta1 = ReadCsv("Theta1_value.csv");
+            var Theta2 = ReadCsv("Theta2_value.csv");
+            var X = ReadCsv("X_value.csv");
+            var y = ReadCsv("Y_value.csv");
+            Matrix<double>[] Theta = new Matrix<double>[2];
+            Theta[0] = Theta1;
+            Theta[1] = Theta2;
+            NeuralNetwork neuralNetwork = new NeuralNetwork()
+            {
+                InputLayerSize = 400,
+                HiddenLayerSize = 25,
+                HiddenLayerLength = 1,
+                OutputLayerSize = 10,
+                TrainingSize = 5000,
+                Lambda = 1
+            };
 
+            neuralNetwork.ReadParams(Theta, X, y);
+
+            double ct = neuralNetwork.Cost();
+
+            Console.ReadLine();
         }
     }
 }
